668d41e40a767f144a8724ca9cefdc11cf244607
Herman Zvonimir Došilović <hermanz.dosilovic@gmail.com>
2019-07-26 00:40:05 +0200

    Create basic API endpoint.

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-import PIL.Image
-import sys
+from io import BytesIO
+
+from PIL import Image
+
+from flask import Flask, request, send_file
 
 LOGO_WIDTH_SCALE_FACTOR = 0.1
 
-if __name__ == "__main__":
-    logo = PIL.Image.open("./images/logo.png", "r")
-    logo_width, logo_height = logo.size
+app = Flask(__name__)
+logo = Image.open("images/logo.png", "r")
+logo_width, logo_height = logo.size
 
-    image = PIL.Image.open(sys.argv[1], "r")
+@app.route("/api/create", methods=["POST"])
+def create():
+    image_file = request.files["image"]
+    image = Image.open(image_file)
+
     image_width, image_height = image.size
     image_longer_side = max(image_width, image_height)
 
@@ -21,4 +28,9 @@ if __name__ == "__main__":
     vertical_offset = int(image_height - resized_logo_height - 0.25 * resized_logo_width)
 
     image.paste(resized_logo, (horizontal_offset, vertical_offset), mask=resized_logo)
-    image.save("result." + image.format.lower())
+
+    image_bytes = BytesIO()
+    image.save(image_bytes, image.format.lower())
+    image_bytes.seek(0)
+
+    return send_file(image_bytes, mimetype=image_file.mimetype)
